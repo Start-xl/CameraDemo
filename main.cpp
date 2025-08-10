69af7898a180da9c6cbb8b67575fe63944c2c570
xl-1/4 <1656119819@qq.com>
2025-08-10 16:21:19 +0800

    添加qss样式文件

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile styleFile(":/style/style.qss");
+    if (styleFile.open(QFile::ReadOnly)) {
+        QString styleSheet = QString(styleFile.readAll());
+        a.setStyleSheet(styleSheet); // 应用样式表
+        styleFile.close();
+    } else {
+        qWarning() << "Failed to load styleSheet:" << styleFile.errorString();
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
